135c2b160eb64413f752f059cb18df476505ffa3
Red22K <ritwik22k@gmail.com>
2024-08-12 12:58:27 +0530

    Created Routes/Views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./Posts");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//! GET home page.
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/profile", isLoggedIn, function (req, res, next) {
+  res.send("Profile");
+});
+
+//! Register User - Post Req
+router.post("/register", async function (req, res) {
+  const {username, email, fullname} = req.body;
+
+  const userData = new userModel({
+    username,
+    email,
+    fullname,
+  });
+
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+//! Login User -
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  async function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  dp: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+  },
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("User", userSchema);

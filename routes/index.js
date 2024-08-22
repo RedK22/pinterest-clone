49fb3c290eb8ef801d2c3a0de5bf712fd62ebb13
Red22K <ritwik22k@gmail.com>
2024-08-22 23:57:50 +0530

    added multer, temp uploading posts, and other things

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./Posts");
 const passport = require("passport");
+const upload = require("./multer");
+
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 
@@ -15,8 +17,12 @@ router.get("/login", function (req, res, next) {
   res.render("login", {error: req.flash("error")});
 });
 
-router.get("/profile", isLoggedIn, function (req, res, next) {
-  res.render("profile");
+router.get("/profile", isLoggedIn, async function (req, res, next) {
+  const user = await userModel
+    .findOne({username: req.session.passport.user})
+    .populate("posts");
+
+  res.render("profile", {user});
 });
 
 //! Register User - Post Req
@@ -57,9 +63,33 @@ router.get("/logout", function (req, res) {
 });
 
 // !Feed
-router.get("/feed", function (req, res) {
-  res.render("feed");
+router.get("/feed", async function (req, res) {
+  const posts = await postModel.find();
+
+  res.render("feed", {posts});
 });
+
+// !Upload Route
+router.post(
+  "/upload",
+  isLoggedIn,
+  upload.single("file"),
+  async function (req, res) {
+    if (!req.file) {
+      return res.status(404).send("No files were uploaded.");
+    }
+    const user = await userModel.findOne({username: req.session.passport.user});
+    const post = await postModel.create({
+      image: req.file.filename,
+      imageText: req.body.filecaption,
+      user: user._id,
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+  }
+);
 
 // ! LoggedIn Function
 function isLoggedIn(req, res, next) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  postText: {
+  imageText: {
+    type: String,
+    required: true,
+  },
+  image: {
     type: String,
     required: true,
   },
